94161f1d243127a1cad615c5ce44f09b58c8785e
Ana Maria Leon Pullas <ana.leon.pullas@udla.edu.ec>
2023-01-31 10:34:09 -0500

    cb2

--- CAD_PROYECTO2/Models/Note.cs
+++ CAD_PROYECTO2/Models/Note.cs
@@ -14,11 +14,12 @@ namespace CAD_PROYECTO2.Models
     {
         [PrimaryKey, AutoIncrement]
         public int IdComentario { get; set; }
-
         public string Filename { get; set; }
         public string Text { get; set; }
         public string Titulo { get; set; }
         public DateTime Date { get; set; }
+
         public int serv { get; set; }
+
     }
 }
